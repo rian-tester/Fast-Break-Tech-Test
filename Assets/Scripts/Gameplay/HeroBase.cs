fea15f2fb00069dd07db5116d4082feecf02e7b8
Fitransyah <fitransyah.rusman@gmail.com>
2025-08-01 22:55:48 +0700

    add turn

--- Assets/Scripts/Gameplay/HeroBase.cs
+++ Assets/Scripts/Gameplay/HeroBase.cs
@@ -7,7 +7,6 @@ public abstract class HeroBase : MonoBehaviour
     private Vector3 lastMoveDirection;
     private float moveSpeed = 5f;
     private Vector2 inputDirection;
-    private Vector3 playerVelocity;
     public float gravity = -9.81f;
     void Awake()
     {
@@ -21,23 +20,21 @@ public abstract class HeroBase : MonoBehaviour
 
     void Update()
     {
-        if (characterController.isGrounded && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f; 
-        }
-
         Vector3 moveDir = new Vector3(inputDirection.x, 0, inputDirection.y);
         if (moveDir.sqrMagnitude > 0.01f)
         {
             lastMoveDirection = moveDir.normalized;
             characterController.Move(lastMoveDirection * Time.deltaTime * moveSpeed);
         }
-
-        playerVelocity.y += gravity * Time.deltaTime; 
+        Turn();       
     }
     
     public void Turn()
     {
-
+        if (lastMoveDirection.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(lastMoveDirection, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+        }
     }
 }
